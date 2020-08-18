795849aa233ef22926fee86affe1ac775602331f
Aaron Cymor Nemeno <anemeno@risingtide.ph>
2020-08-19 07:50:38 +0800

    Mobile UI Complete

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -3,3 +3,5 @@ export 'package:flutter_facebook_responsive_ui/widgets/create_post_container.dar
 export 'package:flutter_facebook_responsive_ui/widgets/rooms.dart';
 export 'package:flutter_facebook_responsive_ui/widgets/profile_avatar.dart';
 export 'package:flutter_facebook_responsive_ui/widgets/stories.dart';
+export 'package:flutter_facebook_responsive_ui/widgets/post_container.dart';
+export 'package:flutter_facebook_responsive_ui/widgets/custom_tab_bar.dart';
